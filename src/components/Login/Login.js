a8fec881e6e83a9bd4b1c736b09c5f1c6199ed32
salihsamet19 <salihsametakar@gmail.com>
2021-01-25 21:14:24 +0300

    Login update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -30,30 +30,34 @@ export default function Login({ setToken }) {
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            <select defaultValue={'DEFAULT'} onChange={e => setUserType(e.target.value)}>
-              <option value="DEFAULT" disabled>Choose personnel type ...</option>
-              <option value="receptionist">Receptionist</option>
+
+      <div class="column">
+            <label class="theLabels">
+            <span  class="float-left">Type: </span>
+              <select class="theInputs" required defaultValue={'DEFAULT'} onChange={e => setUserType(e.target.value)}>
+                <option value="DEFAULT" disabled>Choose personnel type ...</option>
+                <option value="receptionist">Receptionist</option>
               <option value="manager">Manager</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            <p>Username</p>
-            <input type="text" onChange={e => setUserName(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <label>
-            <p>Password</p>
-            <input type="password" onChange={e => setPassword(e.target.value)} />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
+              </select>
+            </label>
+          </div>
+
+          <div class="column">
+            <label class="theLabels">
+              <span class="float-left">Username: </span>
+              <input class="theInputs" type="text" required onChange={e => setUserName(e.target.value)} />
+            </label>
+          </div>
+
+          <div class="column">
+            <label class="theLabels">
+            <span class="float-left">Password: </span>
+              <input class="theInputs" type="text" required onChange={e => setPassword(e.target.value)} />
+            </label>
+          </div>
+          <div class="column">
+            <button type="submit" class="btn btn-info btn-sm">Submit</button>
+          </div>
       </form>
     </div>
   )
