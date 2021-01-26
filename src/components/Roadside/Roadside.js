90668c7541517dc9584ba4eb32ba00a6e9840014
salihsamet19 <salihsametakar@gmail.com>
2021-01-26 03:37:48 +0300

    assistance updates

--- src/components/Roadside/Roadside.js
+++ src/components/Roadside/Roadside.js
@@ -6,7 +6,7 @@ export default function Roadside() {
     <div>
       <h2>Roadside Assistance</h2>
       
-      <div class="flex-container h-300 align-items-center justify-content-space-between flex-direction-row">
+      <div class="flex-container h-300 align-items-center justify-content-space-between flex-direction-row auto-height">
         <div class="width-50">
           <img class="width-60" src={require('./warning.png')} />
         </div>
@@ -15,7 +15,22 @@ export default function Roadside() {
             Please call for assistance.
           </p>
           <p class="font-20">
-            Phone: (0312) 378 38 38
+            24/7 professional support 
+          </p>
+          <p class="font-20">
+            Sending a mechanic to the accident or breakdown site
+          </p>
+          <p class="font-20">
+            Repairing/towing the vehicle
+          </p>
+          <p class="font-20">
+            Providing a replacement vehicle
+          </p>
+          <p class="font-20">
+            Following up repairs
+          </p>
+          <p class="font-20">
+            Phone: <u>(0312) 378 38 38</u>
           </p>
         </div>
         <div class="width-50">
